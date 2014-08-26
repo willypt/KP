155133013e39e04fc12a22c669031957baf138d4
willypt <wilwol@windowslive.com>
2014-08-26 18:32:19 +0700

    asd

--- aplikasi-lan-client/config.php
+++ aplikasi-lan-client/config.php
@@ -1,12 +1,10 @@
 <?php
-
-$conf_ini = parse_ini_file('config.ini');
-
-
 class Config {
-
-	//const hostUrl = "willypt.com/KP/aplikasi-lan-server";
-	const hostUrl = $GLOBALS["conf_ini"]["host"];
+	/**
+	 * Edit host URL here. 
+	 */
+	const hostUrl = "willypt.com/KP/aplikasi-lan-server";
+	
 	public static function getHostUrl(){
 		return 'http://'.self::hostUrl.'/';
 	}
